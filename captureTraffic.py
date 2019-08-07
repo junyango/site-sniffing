2f43ff8819dc475082a4fd4d8f0c65f42483cad2
Ng Jun Yang <ju-nyang@live.com>
2019-08-07 16:36:59 +0800

    error handling and methodology

--- captureTraffic.py
+++ captureTraffic.py
@@ -1,41 +1,53 @@
 import os
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import socket
 import subprocess
 import psutil
 import datetime
 import logging
 import urllib
 import fnmatch
+import socket
+import random
+import sys
+import http.client
 from os.path import expanduser
 from selenium.common.exceptions import InvalidArgumentException
 import pandas as pd
 from urllib.error import HTTPError
 from urllib import request
 from selenium.common.exceptions import TimeoutException
+from fake_useragent import UserAgent
 
-excel_dir = "C:/Users/ju-ny/Desktop/FYPlearn/report_unique_servers2.xlsx"
+excel_dir = "./report_unique_servers2.xlsx"
 df = pd.read_excel(excel_dir, sheet_name="complete_list")
 
-domains = []
+start_index = int(sys.argv[1])
+end_index = int(sys.argv[2])
 
-domain_col = df['Domain']
+s = slice(start_index, end_index)
 
-for row in domain_col:
-    if row not in domains:
-        domains.append(row)
-    else:
-        continue
+ua = UserAgent()
+dictionary = {}
+ip_list = df['IP']
+
+# Initializing the dictionary to be able to retrieve the names easily
+# Different IP (Key) lead to same Domain (Value)
+for index, row in df.iterrows():
+    domain = row['Domain']
+    ip = row['IP']
+
+    dictionary[ip] = domain
+
 
 # Declaring global variable since there is only one instance of chromeDriverPath
 chromeDriverPath = ""
-rootPath = expanduser("~")
+rootPath = expanduser("D:\\")
 sts = ""
 
-logging.basicConfig(filename='application.log', level=logging.INFO, format='%(asctime)s-%(levelname)s-%(message)s')
+logging.basicConfig(filename='capture_traffic.log', level=logging.INFO, format='%(asctime)s-%(levelname)s-%(message)s')
 
-file_path = os.path.join("output/" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+file_path = os.path.join("output/normal_traffic/" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
 if not os.path.exists(file_path):
     os.makedirs(file_path)
 
@@ -47,9 +59,6 @@ def clean_domain(url):
         result = url[7:]
     else:
         result = url
-
-    if result[0:4] == "www.":
-        result = result[4:]
 
     if "/" in result:
         result = result.split("/")[0]
@@ -64,43 +73,46 @@ for root, utils, files in os.walk(rootPath):
         chromeDriverPath = (os.path.join(root, filename))
 
 
-for website in domains:
+for ip in ip_list[s]:
+    # Getting domain
+    domain = dictionary[ip]
+
+    # Initializing instance of chrome driver
     driver = webdriver.Chrome(chromeDriverPath)
+
     # Check if website has http
-    if "http" not in website:
+    if "http" not in domain:
         # appending https:// for urllib
-        domain_urllib = "https://" + website
+        domain_urllib = "https://" + domain
     else:
-        domain_urllib = website
+        domain_urllib = domain
 
-    domain_socket = clean_domain(website)
-
-    # Creating a directory for output
-    datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "/" + domain_socket
-    file_dir = os.path.join(file_path + "/" + domain_socket)
-    if os.path.exists(file_dir):
-        os.mkdir(file_dir)
-
-    # Getting list of possible ip from the particular domain
-    ip_list = []
-    try:
-        socket_info = socket.getaddrinfo(domain_socket, None)
-    except socket.gaierror as e:
-        logging.error(str(e) + str(domain_socket))
-        continue
-
-    for info in socket_info:
-        ip_list.append(info[4][0])
+    # Using fake user agent to prevent HTTP Forbidden error 403
+    # Purpose is just to scrap the links from the given domain
+    # Able to use random user agent instead of Chrome or actual one in Selenium ChromeDriver
+    headers = {'User-Agent': ua.random}
+    req = urllib.request.Request(
+        domain_urllib,
+        data=None,
+        headers={'User-Agent': ua.random}
+    )
 
     # Extracting the html from the website
     try:
-        response = urllib.request.urlopen(domain_urllib)
-    except urllib.error.HTTPError as e:
-        logging.error(str(e) + str(domain_urllib))
+        response = urllib.request.urlopen(req)
+    except urllib.error.HTTPError as httperr:
+        logging.error(str(httperr) + str(domain_urllib))
         continue
-    except urllib.error.URLError as e:
-        logging.error(str(e) + str(domain_urllib))
+    except urllib.error.URLError as urle:
+        logging.error(str(urle) + str(domain_urllib))
         continue
+    except ConnectionResetError as cre:
+        logging.error(str(cre) + str(domain_urllib))
+        continue
+    except TimeoutError as toe:
+        logging.error(str(toe) + str(domain_urllib))
+    except http.client.HTTPException as httpe:
+        logging.error(str(httpe) + str(domain_urllib))
 
     # Scrapping the links from the html page
     links = []
@@ -120,24 +132,36 @@ for website in domains:
     # Might not be too perfect in the case
     abspath = os.path.abspath(file_path)
     interface = "Ethernet"
-    capture_filter = "tcp port 443 and host " + ip_list[0]
-    web_name = domain_socket.split(".")[0]
-    filename = abspath + "\\" + web_name + "_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".pcap"
+    capture_filter = "tcp port 443 and host " + ip
+    filename = abspath + "\\" + domain + "_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".pcap"
 
     # Raw capturing
-    command = ["tshark", "-i", interface, "-c 10000", "-f", capture_filter, "-w", filename]
+    command = ["tshark", "-i", interface, "-a", "duration:120", "-f", capture_filter, "-w", filename]
     sts = subprocess.Popen(command, shell=True)
-    driver.get(domain_urllib)
 
     # Requirement for selenium webdriver to have http:// appended infront otherwise webpage wouldnt load
     # Requirement for socket websites to only have the domain without the "/" and https
     # seleniumLink = links to be clicked by selenium
     # socketLink = links to be resolved by resolve_server function
 
-    countLinks = 0
+    # Ensuring tshark gets to open before continuing with the code
+    while 1:
+        tsharkProcess = os.popen("tasklist | findstr tshark").read()
+        if tsharkProcess == "":
+            continue
+        else:
+            break
 
-    # Providing 1 avenue of termination of breaking the while loop
-    for link in cleanLinks:
+    try :
+        driver.get(domain_urllib)
+    except InvalidArgumentException as iae:
+        logging.info(str(iae) + "Invalid Argument Exception " + str(domain_urllib))
+    except TimeoutException as te:
+        logging.info(str(te) + "Time Out Exception " + str(domain_urllib))
+
+    # Implementing a do while loop
+    # While tshark is still running, continue automating the browser
+    while 1:
         tsharkProcess = os.popen("tasklist | findstr tshark").read()
         # Kill all processes when capturing is done
         if tsharkProcess == "":
@@ -145,51 +169,53 @@ for website in domains:
             driver.quit()
             logging.info("Done with testing... Killing cmd and dumpcap now...")
             # Remove the instance of chromedriver.exe from running processes
-            os.system("taskkill  /F /pid "+str(sts.pid))
+            os.system("taskkill  /F /pid " + str(sts.pid))
 
             for proc in psutil.process_iter():
                 try:
                     # Get process name & pid from process object to kill
                     processName = proc.name()
                     if "dumpcap" in processName:
-                        os.system("taskkill  /F /pid "+str(proc.pid))
+                        os.system("taskkill  /F /pid " + str(proc.pid))
                 except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                     pass
-
             break
-
-        ip_socket = []
-        if "http" not in link and ".com" not in link:
-            seleniumLink = "http://" + domain_socket + link
-            socketLink = domain_socket
         else:
-            seleniumLink = link
-            socketLink = clean_domain(link)
+            if len(cleanLinks) > 1:
+                link = random.choice(cleanLinks)
+                ip_socket = []
+                if "http" not in link and ".com" not in link:
+                    seleniumLink = "http://" + domain + link
+                    socketLink = domain
+                else:
+                    seleniumLink = link
+                    socketLink = clean_domain(link)
 
-        try:
-            socket_info = socket.getaddrinfo(socketLink, None)
-        except socket.gaierror as e:
-            logging.error(str(e) + " error for " + str(socketLink))
-            continue
-        except UnicodeError as e:
-            logging.error(str(e) + " error for " + str(socketLink))
-            continue
-
-        for info in socket_info:
-            ip_socket.append(info[4][0])
-
-        for ip in ip_socket:
-            if ip in ip_list:
                 try:
-                    driver.get(seleniumLink)
-                    logging.info("Successfully accessed website " + str(seleniumLink))
-                    countLinks = countLinks + 1
-                except InvalidArgumentException as iae:
-                    logging.info(str(iae) + "Invalid Argument Exception " + str(seleniumLink))
-                except TimeoutException as te:
-                    logging.info(str(te) + "Time Out Exception " + str(seleniumLink))
-                finally:
-                    break
+                    socket_info = socket.getaddrinfo(socketLink, None)
+                except socket.gaierror as e:
+                    logging.error(str(e) + " error for " + str(socketLink))
+                    continue
+                except UnicodeError as e:
+                    logging.error(str(e) + " error for " + str(socketLink))
+                    continue
+
+                for info in socket_info:
+                    ip_socket.append(info[4][0])
+
+                for ip_test in ip_socket:
+                    if ip_test == ip:
+                        try:
+                            driver.get(seleniumLink)
+                            logging.info("Successfully accessed website " + str(seleniumLink))
+                        except InvalidArgumentException as iae:
+                            logging.info(str(iae) + "Invalid Argument Exception " + str(seleniumLink))
+                        except TimeoutException as te:
+                            logging.info(str(te) + "Time Out Exception " + str(seleniumLink))
+                        finally:
+                            break
+                    else:
+                        continue
             else:
                 continue
 
