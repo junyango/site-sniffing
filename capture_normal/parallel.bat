236801b9e749f0c4c14507d4aa835552cc1d5474
JunYang <ju-nyang@live.com>
2019-08-25 09:09:29 -0700

    Changed parallel script

--- capture_normal/parallel.bat
+++ capture_normal/parallel.bat
@@ -1,15 +1,15 @@
 @echo off
 REM to concurrently start 10 instances of chrome to collect normal traffic
 
-python D:\junyang\site-sniffing\capture_normal\captureTraffic.py -s 0 -e 4999
-python D:\junyang\site-sniffing\capture_normal\captureTraffic.py -s 5000 -e 9999
-python D:\junyang\site-sniffing\capture_normal\captureTraffic.py -s 10000 -e 19999
-python D:\junyang\site-sniffing\capture_normal\captureTraffic.py -s 20000 -e 24999
-python D:\junyang\site-sniffing\capture_normal\captureTraffic.py -s 25000 -e 29999
-python D:\junyang\site-sniffing\capture_normal\captureTraffic.py -s 30000 -e 34999
-python D:\junyang\site-sniffing\capture_normal\captureTraffic.py -s 35000 -e 39999
-python D:\junyang\site-sniffing\capture_normal\captureTraffic.py -s 40000 -e 44999
-python D:\junyang\site-sniffing\capture_normal\captureTraffic.py -s 45000 -e 49999
-python D:\junyang\site-sniffing\capture_normal\captureTraffic.py -s 50000 -e 58202
+start cmd /k python C:\Users\Traffic_collection\Desktop\site-sniffing\capture_normal\captureTraffic.py -s 0 -e 4999
+start cmd /k python C:\Users\Traffic_collection\Desktop\site-sniffing\capture_normal\captureTraffic.py -s 5000 -e 9999
+start cmd /k python C:\Users\Traffic_collection\Desktop\site-sniffing\capture_normal\captureTraffic.py -s 10000 -e 19999
+start cmd /k python C:\Users\Traffic_collection\Desktop\site-sniffing\capture_normal\captureTraffic.py -s 20000 -e 24999
+start cmd /k python C:\Users\Traffic_collection\Desktop\site-sniffing\capture_normal\captureTraffic.py -s 25000 -e 29999
+:: start cmd /k python C:\Users\Traffic_collection\Desktop\site-sniffing\capture_normal\captureTraffic.py -s 30000 -e 34999
+:: start cmd /k python C:\Users\Traffic_collection\Desktop\site-sniffing\capture_normal\captureTraffic.py -s 35000 -e 39999
+:: start cmd /k python C:\Users\Traffic_collection\Desktop\site-sniffing\capture_normal\captureTraffic.py -s 40000 -e 44999
+:: start cmd /k python C:\Users\Traffic_collection\Desktop\site-sniffing\capture_normal\captureTraffic.py -s 45000 -e 49999
+:: start cmd /k python C:\Users\Traffic_collection\Desktop\site-sniffing\capture_normal\captureTraffic.py -s 50000 -e 58202
 
 pause
